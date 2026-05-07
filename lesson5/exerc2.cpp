5b018d210666b6ce4344b06ecbc3fee34400d365
timothy1498 <74752830+TIMOTHY1498@users.noreply.github.com>
2026-05-07 16:12:13 +0700

    Update exerc2.cpp

--- lesson5/exerc2.cpp
+++ lesson5/exerc2.cpp
@@ -11,15 +11,17 @@ int main() {
     cout << "\n";
 
     if (numid == 1) {
-        cout << "idk" << endl;
+        cout << "season is winter" << endl;
     } else if (numid == 2) {
-        cout << "idk" << endl;
+        cout << "season is winter" << endl;
     } else if (numid == (3 > 5 ? 3 : 5)) {
         cout << "season is spring" << endl;
     } else if (numid == (6 > 8 ? 6 : 8)) {
+        cout << "season is summer" << endl;
+    } else if (numid == (9 > 11 ? 9 : 11)) {
         cout << "season is autumn" << endl;
-    } else if (numid == (6 > 8 ? 6 : 8)) {
-        cout << "you entered five!" << endl;
+    } else if (numid == (9 > 11 ? 9 : 11)) {
+        cout << "season is winter" << endl;
     } else {
         cout << "unknown" << endl;
     }
