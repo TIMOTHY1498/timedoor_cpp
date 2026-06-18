d1755b2584ea68e5683aaa6fa4e2fb6017cc0710
TIMOTHY1498 <timothyb.s@outlook.co.id>
2026-06-18 16:15:14 +0700

    useless phuck

---
+++ import agus.py
@@ -0,0 +1,11 @@
+helloWorld = print
+helloWorld("Hello, World!")
+
+i = "World, Hello!"
+
+helloWorld(i+" This is Agus.")
+clear = lambda: print("\033[H\033[J", end="")
+
+clear()
+print("This is Agus. I am a Python programmer.")
+print("I love coding and making shitty new projects.")
