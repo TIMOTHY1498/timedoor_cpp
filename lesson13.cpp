8fa2f8ae97f61bc1f940d28d7d775deb06961e90
TIMOTHY1498 <timothyb.s@outlook.co.id>
2026-08-27 17:12:51 +0700

    oop function public private

--- lesson13.cpp
+++ lesson13.cpp
@@ -10,8 +10,9 @@ class Student {
         int StudentNumber;
         int YearEnter;
         std::string Gender;
+
     public:
-        void printInformation() {
+        void printStudentBasicInformation() {
             std::cout << "======[ STUDENT INFO ]=======\n";
             std::cout << "Name     : " << Name << "\n";
             std::cout << "Age      : " << Age << "\n";
@@ -19,6 +20,27 @@ class Student {
             std::cout << "IsActive : " << std::boolalpha << IsActive << "\n";
         };
 
+        int getStudentActiveYearTime() {
+            std::time_t now = std::time(nullptr);
+            std::tm* local_time = std::localtime(&now);
+
+            int CurrentYear = local_time->tm_year + 1900; // get current year
+            local_time = nullptr;
+
+            return CurrentYear - YearEnter;
+        }
+
+        int printStudentActiveInfo() {
+            if (IsActive == true) {
+                std::cout << "The student still active learning" << "\n";
+            } else if (IsActive == false) {
+                std::cout << "The student has already graduated and inactive right now" << "\n";
+            } else {
+                std::cout << "The student status is unknown\n";
+            }
+        }
+
+        // cool constructor epik
         Student(std::string name2, int age2, double height2, bool isActive2, int yearEnter2, int studentNum2, std::string gender2) {
             this->Name = name2;
             this->Age = age2;
@@ -41,5 +63,7 @@ int main() {
         "Male"
     );
 
-    timothy.printInformation();
+    timothy.printStudentBasicInformation();
+    std::cout << "Active since " << timothy.getStudentActiveYearTime() << " years ago\n";
+    timothy.printStudentActiveInfo();
 }
